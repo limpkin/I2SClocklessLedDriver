3b030e760b8a933bf3bae4c66dce90bb6271a3b5
ewowi <ewowi@icloud.com>
2025-10-26 12:19:50 +0100

    Add clang-format, Remove COLOR_ORDER_ compiler directives
    Add .clang-format so option shift F formats the files and manually applied to all files
    
    Remove COLOR_ORDER_ compiler directives as .cpp compiles .h before it is included so cannot set before. Needs to be set explicitly in initled()
    
    initled() without color order does GRB as default (WS2812), use overloaded functions to set other color configs

--- src/I2SClocklessLedDriver.cpp
+++ src/I2SClocklessLedDriver.cpp
@@ -13,78 +13,72 @@
 
 // IDF5.5: __NB_DMA_BUFFER and NUM_STRIPS are global variables to allow changing it at runtime
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 5, 0)
-    uint8_t __NB_DMA_BUFFER = 6;
-    uint8_t NUM_STRIPS = 16;
+uint8_t __NB_DMA_BUFFER = 6;
+uint8_t NUM_STRIPS = 16;
 #endif
 
 #ifdef CONFIG_IDF_TARGET_ESP32S3
-    clock_speed clock_1123KHZ = {4, 20, 9};
-    clock_speed clock_1111KHZ = {4, 2, 1};
-    clock_speed clock_1000KHZ = {5, 1, 0};
-    clock_speed clock_800KHZ = {6, 4, 1};
+clock_speed clock_1123KHZ = {4, 20, 9};
+clock_speed clock_1111KHZ = {4, 2, 1};
+clock_speed clock_1000KHZ = {5, 1, 0};
+clock_speed clock_800KHZ = {6, 4, 1};
 #endif
 
 // IDF5.5: updateLeds
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 5, 0)
-    void I2SClocklessLedDriver::updateDriver(uint8_t *Pinsq, uint16_t *sizes, int num_strips, uint8_t dmaBuffer, int nb_components, int p_r, int p_g, int p_b) {
+void I2SClocklessLedDriver::updateDriver(uint8_t* Pinsq, uint16_t* sizes, int num_strips, uint8_t dmaBuffer, int nb_components, int p_r, int p_g, int p_b) {
+  // do what ledsDriver.initled is doing, except i2sInit
 
-        //do what ledsDriver.initled is doing, except i2sInit
+  // from initled
+  this->num_strips = num_strips;
+  total_leds = 0;
+  for (int i = 0; i < num_strips; i++) {
+    stripSize[i] = sizes[i];
+    total_leds += sizes[i];
+  }
+  uint16_t num_led_per_strip = maxLength(sizes, num_strips);
 
-        //from initled
-        this->num_strips = num_strips;
-        total_leds = 0;
-        for (int i = 0; i < num_strips; i++)
-        {
-            stripSize[i] = sizes[i];
-            total_leds += sizes[i];
-        }
-        uint16_t num_led_per_strip = maxLength(sizes, num_strips);
+  this->nb_components = nb_components;
+  this->p_r = p_r;
+  this->p_g = p_g;
+  this->p_b = p_b;
 
-        this->nb_components = nb_components;
-        this->p_r = p_r;
-        this->p_g = p_g;
-        this->p_b = p_b;
+  // from __initled:
 
-        //from __initled:
+  this->num_led_per_strip = num_led_per_strip;
+  _offsetDisplay.offsetx = 0;
+  _offsetDisplay.offsety = 0;
+  _offsetDisplay.panel_width = num_led_per_strip;
+  _offsetDisplay.panel_height = 9999;
+  _defaultOffsetDisplay = _offsetDisplay;
+  linewidth = num_led_per_strip;
 
-        this->num_led_per_strip = num_led_per_strip;
-        _offsetDisplay.offsetx = 0;
-        _offsetDisplay.offsety = 0;
-        _offsetDisplay.panel_width = num_led_per_strip;
-        _offsetDisplay.panel_height = 9999;
-        _defaultOffsetDisplay = _offsetDisplay;
-        linewidth = num_led_per_strip;
+  // setShowDelay(num_led_per_strip);
+  setShowDelay();
+  setGlobalNumStrips();
 
-        // setShowDelay(num_led_per_strip);
-        setShowDelay();
-        setGlobalNumStrips();
+  setPins(Pinsq);  // if pins and lengths changed, set that right
 
-        setPins(Pinsq); //if pins and lengths changed, set that right
+  // i2sInit(); //not necessary, initled did it, no need to change
 
-        // i2sInit(); //not necessary, initled did it, no need to change
+  deleteDriver();  // free previous allocations
 
-        deleteDriver(); //free previous allocations
+  __NB_DMA_BUFFER = dmaBuffer;  // set new buffer count
 
-        __NB_DMA_BUFFER = dmaBuffer; //set new buffer count
+  initDMABuffers();  // create them again
 
-        initDMABuffers(); //create them again
+  ESP_LOGD(TAG, "updateLeds %d x %d (%d)", num_strips, num_led_per_strip, __NB_DMA_BUFFER);
+}
 
-        ESP_LOGD(TAG, "updateLeds %d x %d (%d)", num_strips, num_led_per_strip, __NB_DMA_BUFFER);
-
-    }
-
-    void I2SClocklessLedDriver::deleteDriver() {
-
-        #if CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32 //P4 for PhysicalDriver not supported yet
-            for (int i = 0; i < __NB_DMA_BUFFER + 2; i++)
-            {
-                heap_caps_free(DMABuffersTampon[i]->buffer);
-                heap_caps_free(DMABuffersTampon[i]);
-            }
-            heap_caps_free(DMABuffersTampon);
-        #endif
-        //anything else to delete? I2S ...
-
-    }
+void I2SClocklessLedDriver::deleteDriver() {
+  #if CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32  // P4 for PhysicalDriver not supported yet
+  for (int i = 0; i < __NB_DMA_BUFFER + 2; i++) {
+    heap_caps_free(DMABuffersTampon[i]->buffer);
+    heap_caps_free(DMABuffersTampon[i]);
+  }
+  heap_caps_free(DMABuffersTampon);
+  #endif
+  // anything else to delete? I2S ...
+}
 
 #endif
